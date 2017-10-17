464ca083e1e1dbc3d8c7216b2b1ac9ae0aa30801
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2017-10-17 12:33:17 -0600

    mau

--- view/header.php
+++ view/header.php
@@ -6,15 +6,13 @@
 	<!--Metadato responsivo de Bootstrap-->
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<!--Archivos CSS-->
-	
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  -->
-
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  
+<!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.js" />
-<link rel="stylesheet" href="assets/js/bootstrap.min.js" />
+<link rel="stylesheet" href="assets/js/bootstrap.min.js" /> -->
 
         
    
