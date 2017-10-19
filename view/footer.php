c76c748ed8fcf876faaf445cb892677e5904e772
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2017-10-19 00:12:14 -0600

    mau

--- view/footer.php
+++ view/footer.php
@@ -1,15 +1,7 @@
   
   
 
-<style>
 
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-  </style>
 		<footer class="form-horizontal text-center">
 			<p>DERECHOS RESERVADOS MUNICIPALIDAD DE NICOYA, PLANIFICACIÓN URBANA ®</p>
 			<div class="w3-container w3-theme-l1">
--- view/header.php
+++ view/header.php
@@ -7,74 +7,53 @@
     <title>SCRPU</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
-</head>
 
-<body>
-        <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
+
+    <style>
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
+    .row.content {height: 550px}
     
     /* Set gray background color and 100% height */
     .sidenav {
-      padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
+        
+    /* On small screens, set height to 'auto' for the grid */
     @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height:auto;} 
+      .row.content {height: auto;} 
     }
   </style>
-<nav class="navbar navbar-inverse">
+
+</script>
+<script>
+$(document).ready(function(){
+    $("puesto").click(function(){
+        $("#div1").load("?c=class03puestos");
+    });
+});
+</script>
+
+
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse visible-xs">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#accordion">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">SCRPU</a>
+      <a class="navbar-brand" >SCRPU</a>
     </div>
-    <div class="collapse navbar-collapse" id="accordion">
+    <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Inicio<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li> 
-        <li><a href="#">Page 3</a></li> 
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-   <!-- sidenav acordion -->
-   
-
- <div class="container-fluid text-center">    
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-         <div class="panel-group" id="accordion">
+        <li class="active"><a href="#">Dashboard</a></li>
+        <div class="panel-group" id="accordion">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
@@ -128,20 +107,9 @@
                                 </tr>
                                 <tr>
                                     <td>
-                                        <script>
-                                           function hacer_click()
-    {
-        $(document).ready(function(){
-    $("a").click(function(){
-        $("#wee").load("?c=class03puestos");
-    });
-});
-    }
-
-
-</script>
+                            
                                           <span class="glyphicon glyphicon-list-alt"></span>
-                                         <a onclick="hacer_click()">Puestos</a>
+                                         <a  href="?c=class03puestos">Puestos</a>
                                     </td>
                                 </tr>
                                 <tr>
@@ -314,18 +282,258 @@
                     </div>
                 </div>
             </div>
-    </div>
-    <div class="col-sm-8 text-left" id="wee"> 
-     
-    </div>
-    <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>ADS</p>
-      </div>
-      <div class="well">
-        <p>ADS</p>
-      </div>
+      </ul>
     </div>
   </div>
-</div>
+</nav>
+
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav hidden-xs">
+      <h2>SCRPU</h2>
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Dashboard</a></li>
+       <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseUno"><span class="glyphicon glyphicon-folder-close">
+                            </span>Menu</a>
+                        </h4>
+                    </div>
+                    <div id="collapseUno" class="panel-collapse collapse in ">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-pencil text-primary"></span><a href="?c=class0102usuarios">Articles</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-flash text-success"></span><a href="http://www.jquery2dotnet.com">News</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-file text-info"></span><a href="http://www.jquery2dotnet.com">Newsletters</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-comment text-success"></span><a href="http://www.jquery2dotnet.com">Comments</a>
+                                        <span class="badge">42</span>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseDos"><span class="glyphicon glyphicon-th">
+                            </span>Mantenimientos</a>
+                        </h4>
+                    </div>
+                    <div id="collapseDos" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-user"></span>
+                                         <a href="?c=class0102usuarios">Usuarios</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                            
+                                          <span class="glyphicon glyphicon-list-alt"></span>
+                                         <a id="puesto"  href="">Puestos</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-tags"></span>
+                                         <a href="?c=class06actdes">Actividades a Desarrollar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-list-alt"></span>
+                                         <a href="?c=class07terrft">Terreno Frete a Ruta</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-map-marker"></span>
+                                         <a href="?c=class08regcor">Localización</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-list-alt"></span>
+                                         <a href="?c=class09desceg">Espacio Geográfico</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-leaf"></span>
+                                         <a href="?c=class10aspbio">Aspectos Biofísicos</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-briefcase"></span>
+                                         <a href="?c=class12tipdesec">Tipo de Desarrollo en el Sector</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-globe"></span>
+                                         <a href="?c=class13aarep">Afectación de Áreas de protección</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-flash"></span>
+                                         <a href="?c=class16servae">Servicio de Electricidad y Agua</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-road"></span>
+                                         <a href="?c=class18calleser">Existencia de Calle en Servidumbre Frente a Ruta</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-list-alt"></span>
+                                         <a href="?c=class20desas">Desarrolo de Servidumbre</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-refresh"></span>
+                                         <a href="?c=class22serrvi">Servicio de Red Vial</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-link"></span>
+                                         <a href="?c=class24infest">Existencia de Infraestructura</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-send"></span>
+                                         <a href="?c=class26planreg">Plan Retulador</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-tree-conifer"></span>
+                                         <a href="?c=class32capuso">Capacidad de Uso de Suelo</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-italic"></span>
+                                         <a href="?c=class34clases">Clases</a>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                         <span class="glyphicon glyphicon-tree-deciduous"></span>
+                                         <a href="?c=class35tipsue">Tipo Suelo</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTres"><span class="glyphicon glyphicon-user">
+                            </span>Account</a>
+                        </h4>
+                    </div>
+                    <div id="collapseTres" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <a href="http://www.jquery2dotnet.com">Change Password</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a href="http://www.jquery2dotnet.com">Notifications</a> <span class="label label-info">5</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a href="http://www.jquery2dotnet.com">Import/Export</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-trash text-danger"></span><a href="http://www.jquery2dotnet.com" class="text-danger">
+                                            Delete Account</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseCuatro"><span class="glyphicon glyphicon-file">
+                            </span>Reports</a>
+                        </h4>
+                    </div>
+                    <div id="collapseCuatro" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-usd"></span><a href="http://www.jquery2dotnet.com">Sales</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-user"></span><a href="http://www.jquery2dotnet.com">Customers</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-tasks"></span><a href="http://www.jquery2dotnet.com">Products</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-shopping-cart"></span><a href="http://www.jquery2dotnet.com">Shopping Cart</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </ul><br>
+    </div>
+
+
+   <div class="col-sm-9">
+    <br>
+   <div class="well" id="div1">
+        
+      </div>
+    </div>
+
+
+
+  </div>
 </div>
