586b6679f19ccde13d8973a1a4f63eb56bc7499c
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2018-02-28 22:23:44 -0600

    cambios varios

--- view/class04inspeccion/editarPermisos.php
+++ view/class04inspeccion/editarPermisos.php
@@ -1,73 +1,66 @@
+ <?php $idactividades = $this->pu04inspeccion->getTodasActividades(); ?>
 
-  <?php $idactividades = $this->pu04inspeccion->getTodasActividades(); ?>
-
-
-
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Panel de Administración</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
-    <link href="../css/dashboard.css" rel="stylesheet">
-  </head>
-
-  <body>
-  
-
-    <div class="container-fluid">
-      <div class="row">
-       
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Panel de Administración</h1>
-
-          <h4 class="sub-header">Modificando trámites</h4>
-          <p>Se están modificando los permisos para el trámite:</p> 
-
-          <div class="row">
-            <div class="col-sm-6 col-sm-offset-3">
-            
-            <div class="panel panel-default">
-              <div class="panel-heading"><h3 class="panel-title">Edición de permisos</h3></div>
-              <div class="panel-body">
-               <div class="form-group">
-                <form method="POST" action="?c=class04inspeccion&m=editarActividades">
-        <label for="id">Código Trámite</label>
-        <input type="text" class="form-control" id="id" name="id" value="<?php echo $this->pu04inspeccion->getAtributo('PU04IDTRA');?>" readonly>
-        <?php  $idtramite = $this->pu04inspeccion->getAtributo('PU04IDTRA'); ?>
-      </div>
-        <?php foreach( $idactividades as $idaspecto ): ?>
-          <?php $isCheck = $this->pu04inspeccion->tieneActividades($idtramite, $idaspecto['PU06IDACTDES']);?>
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" name="idactdes<?php echo $idaspecto['PU06IDACTDES']; ?>"
-                        <?php if($isCheck['total']) {echo "checked";} ?>/> <?php echo $idaspecto['PU06DESAD'] ;?> 
-                    </label>                                          
-                  </div>
-        <?php 
-        	endforeach; 
-        
-        ?>
-                  <button type="submit" class="btn btn-default">Guardar</button>
-                </form>
-              </div>
-            </div>
+<br>
+<center>
+<h2>Inspección del Tramites</h2>
+<label for="id">Código Trámite</label>
+<div class="col-xs-2">
+  <input type="text" class="form-control" id="id" name="id" value="<?php echo $this->pu04inspeccion->getAtributo('PU04IDTRA');?>" readonly> <?php  $idtramite = $this->pu04inspeccion->getAtributo('PU04IDTRA'); ?>
+</div>
+</center><br>
+<div id="parentVerticalTab">
+  <ul class="resp-tabs-list hor_1">
+    <li href="#tabconten1">*Espacio geografico*</li>
+    <li href="#tabconten2">Aspectos</li>
+    <li href="#tabconten3">Areas de Proteccion</li>
+    <li href="#tabconten4">vistaEditactdes</li>
+    <li href="#tabconten5">Tramites Aceptados</li>
+    <li href="#tabconten6">Tramites Denegados</li>
+  </ul>
+  <div class="resp-tabs-container hor_1">
+    <div class="container-fluid" id="tabconten1">
+      <!-- contenido de tab 1 -->
+                    <form method="POST" action="?c=class04inspeccion&m=editarActividades">
+          <div class="form-group">
+            <label for="id">Código Trámite</label>
+            <input type="text" class="form-control" id="id" name="id" value="<?php echo $this->pu04inspeccion->getAtributo('PU04IDTRA');?>" readonly> <?php  $idtramite = $this->pu04inspeccion->getAtributo('PU04IDTRA'); ?>
           </div>
-            
-        </div>
+          <?php foreach( $idactividades as $idaspecto ): ?>
+          <?php $isCheck = $this->pu04inspeccion->tieneActividades($idtramite, $idaspecto['PU06IDACTDES']);?>
+          <div class="checkbox">
+            <label>
+            <input type="checkbox" name="idactdes<?php echo $idaspecto['pu06idactdes']; ?>"
+             <?php if($isCheck['total']) {echo "checked";} ?>
+            /> <?php echo $idaspecto['PU06DESAD'] ;?>
+            </label>
+          </div>
+          <?php endforeach; ?>
+          <button type="submit" class="btn btn-default">Guardar</button>
+          <br>
+        </form>
 
-        </div>
-      </div>
     </div>
+    <div class="container-fluid" id="tabconten2">
+      <!-- contenido de tab 2 -->
+    </div>
+    <div class="container-fluid" id="tabconten3">
+      <!-- contenido de tab 3 -->
+    </div>
+    <div class="container-fluid" id="tabconten4">
+      <!-- contenido de tab 4 -->
+    </div>
+    <div class="container-fluid" id="tabconten5">
+      <!-- contenido de tab 5 -->
+    </div>
+    <div class="container-fluid" id="tabconten6">
+      <!-- contenido de tab 6 -->
+    </div>
+  </div>
+</div>
+<br/>
+<div id="nested-tabInfo2">
+   Selected tab: <span class="tabName"></span>
+</div>
+<br/>
+<br/>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  </body>
-</html>
