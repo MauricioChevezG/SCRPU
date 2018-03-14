c1a1831a85e733b82a7574721388dd5eb9cf8319
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2018-03-13 18:43:16 -0600

    mau

--- view/class04ingresotramite/agregar.php
+++ view/class04ingresotramite/agregar.php
@@ -1,10 +1,24 @@
    
-    <center><h2>Agregar Nuevo Trámite</h2></center>
+   <div class="container-fluid">
+     
+  <center><h2>Agregar Nuevo Trámite</h2></center>
     <form action="?c=class04ingresotramite&m=agregar" method="post" >
-      <div class="form-group">
+      <div class="form-group row">
+        <div class="col-sm-4">
         <label for="PU04IDTRA">Código del Trámite:</label>
         <input type="text" class="form-control" id="PU04IDTRA" name="PU04IDTRA">
+        </div>
       </div>
+
+      <div class="container-fluid">
+      <div class="form-group row">
+      <div class="col-xs-2">
+      <label for="PU04FETRA">Fecha:</label>
+      <input type="text" class="form-control" name="PU04FETRA" id="fecha">
+      </div>
+      </div>
+      </div>
+
       <div class="form-group">
         <label for="PU04DESCRIPCIONLUGAR">Dirección del trámite a realizar:</label>
         <input type="text" class="form-control" id="PU04DESCRIPCIONLUGAR" name="PU04DESCRIPCIONLUGAR">
@@ -12,6 +26,6 @@
 
       <button type="submit" class="btn btn-success">Guardar Trámite</button> 
       <a id="regresar" class="btn btn-danger" role="button" href="?c=class04ingresotramite&m=index">Regresar</a>  
-
-     
      </form>
+
+</div>
